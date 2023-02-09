d0662f520cb62dd9c3439bd3f893d2f110d234bd
郭梓繁 <826148267@qq.com>
2023-02-09 22:05:07 +0800

    ...

--- gdbigdata-accessmiddleserver/src/main/java/edu/jnu/controller/UserApi.java
+++ gdbigdata-accessmiddleserver/src/main/java/edu/jnu/controller/UserApi.java
@@ -11,7 +11,6 @@ import io.swagger.annotations.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
